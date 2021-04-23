e8c344ade0aad2d48bb8191f19852b6aa73b49b5
jaronwhipple2022 <77037570+jaronwhipple2022@users.noreply.github.com>
2021-04-23 14:27:25 -0600

    javascript update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+
+// Array of links that will be updated as new assignments are completed.
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week 1",
+      url: "week1/index.html"
+    },
+    {
+      label: "Week 1",
       url: "week1/index.html"
     }
   ]
+
+links.forEach(appendListItem);
+
+function appendListItem(label, url) {
+  document.getElementById("assignmentList").innerHTML += "<li>" + label + " <a href=" + url + "></li>" + "<br>";
+  };
+
+  //<li>Week 1:<a href="Week 1/index.html" target="_blank">Week 1</a></li>
+//<li>Week 2:<a href="Week 2/index.html" target="_blank">Week 2 coming soon...</a></li>
